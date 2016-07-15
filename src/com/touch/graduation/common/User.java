268d97baf594c8311401156a7e04a01ed01216a7
liunianmiyu <sdaujsjzcc@163.com>
2016-07-15 14:12:59 +0800

    add user bean

---
+++ src/com/touch/graduation/common/User.java
@@ -0,0 +1,24 @@
+package com.touch.graduation.common;
+
+public class User {
+
+	private String name;
+	private String passwd;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+}
